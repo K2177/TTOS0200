877d920df57309ce9f7ab6fae19514720f194cf5
K2177 <K2177@student.jamk.fi>
2017-09-12 21:04:35 +0300

    Teht 1 tehty

---
+++ Labra1+2/Tht01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Labra1_2
+{
+    class Tehtava1
+    {
+        public static void Main()
+        {
+            // Tee ohjelma, joka tulostaa käyttäjän antamaa lukua (1, 2 tai 3) vastaavan luvun sanana (yksi, kaksi tai kolme).
+            // Jos käyttäjä syöttää jonkin muun luvun, tulee näytölle tulostaa teksti: "joku muu luku".
+
+            Console.WriteLine("Anna luku: ");
+            int luku = Convert.ToInt32(Console.ReadLine());
+            if (luku == 1)
+            {
+                Console.WriteLine("Annoit luvun yksi");
+            }
+            else if (luku == 2)
+            {
+                Console.WriteLine("Annoit luvun kaksi");
+            }
+            else if (luku == 3)
+            {
+                Console.WriteLine("Annoit luvun kolme");
+            }
+            else
+            {
+                Console.WriteLine("Annoit jonkin muun luvun");
+            }
+        }
+    }
+}
