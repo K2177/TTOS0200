e0d70b8bfca6c9c67a29a6c73eeb35dfcb61e6b1
K2177 <K2177@student.jamk.fi>
2017-09-20 10:52:53 +0300

    1 aloitus

---
+++ Labra4/Tht01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra4
+{
+    class Class
+    {
+        public static bool OnkoLuku(string syote)
+        {
+
+        }
+        public static bool OnkoPvm(string syote)
+        {
+
+        }
+    }
+}
