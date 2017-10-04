96bcf40d34559f54c296878df90d86e9ae40aeb6
K2177 <K2177@student.jamk.fi>
2017-10-04 03:31:08 +0300

    Tht 4 & 5

---
+++ Labra4/Tht04.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Labra4
+{
+    class Staff
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public int Salary { get; set; }
+        public Staff(string name)
+        {
+            Name = Name;
+        }
+        public Staff(string name, string profession, int salary)
+        {
+            Name = name;
+            Profession = profession;
+            Salary = salary;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Profession + ", Salary: " + Salary;
+        }
+    }
+    class Boss : Staff
+    {
+        public string Car { get; set; }
+        public int Bonus { get; set; }
+        public Boss(string name, string profession, int salary, string car, int bonus) : base(name, profession, salary)
+        {
+            Car = car;
+            Bonus = bonus;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Profession + ", Salary: " + Salary + ", Car: " + Car + ", Bonus: " + Bonus;
+        }
+    }
+    class Tht04
+    {
+        static void Tehtava4()
+        {
+            Staff person = new Staff("Kalle Kalamies", "Duunari", 1200);
+            Console.WriteLine("Employee:\n" + person);
+            Staff boss = new Boss("Matti Majava", "Pomo", 4400, "Ferrari", 2500);
+            Console.WriteLine("Boss:\n" + boss);
+            Staff person2 = new Staff("Timo Tilli", "Työmies", 1400);
+            Console.WriteLine("Employee:\n" + person2);
+        }
+    }
+}
---
+++ Labra4/Tht05.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labra4
+{
+    class Vehicles
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public Vehicles(string name, string model, int year, string color)
+        {
+            Name = name;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Model: " + Model + ", Year: " + Year + ", Color: " + Color;
+        }
+    }
+    class Bikes : Vehicles
+    {
+        public string GearWheels { get; set; }
+        public string GearName { get; set; }
+        public Bikes(string name, string model, int year, string color, string gearWheels, string gearName) : base(name, model, year, color)
+        {
+            GearWheels = gearWheels;
+            GearName = gearName;
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Model: " + Model + ", Year: " + Year + ", Color: " + Color + ", GearWheels: " + GearWheels + ", Gear Name: " + GearName;
+        }
+    }
+    class Boats : Vehicles
+    {
+        public string Type { get; set; }
+        public int Seats { get; set; }
+        public Boats(string name, string model, int year, string color, string type, int seats) : base(name, model, year, color)
+        {
+            Type = type;
+            Seats = seats;
+        }
+        public override string ToString()
+        {
+            return "Name: " + Name + ", Model: " + Model + ", Year: " + Year + ", Color: " + Color + ", Type: " + Type + ", Seats: " + Seats;
+        }
+    }
+    class Tht05
+    {
+        static void Main()
+        {
+            Vehicles bike1 = new Bikes("Jopo", "Street", 2016, "Blue", "False", "");
+            Console.WriteLine("Bike info\n" + bike1);
+            Vehicles bike2 = new Bikes("Tunturi", "StreetPower", 2010, "Black", "True", "Shimano");
+            Console.WriteLine("Bike2 info\n" + bike2);
+            Vehicles boat1 = new Boats("Suvi", "S900", 1990, "White", "Rowboat", 3);
+            Console.WriteLine("Boat info\n" + boat1);
+            Vehicles boat2 = new Boats("Yamaha", "1000", 2010, "Yellow", "Motorboat", 5);
+            Console.WriteLine("Boat2 info\n" + boat2);
+        }
+    }
+}
