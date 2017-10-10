7ca170440b5817372a956d20d99f80ae1cfd1775
K2177 <K2177@student.jamk.fi>
2017-10-11 00:00:19 +0300

    teht tehty

---
+++ Labra6/Tht01.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Labra6
+{
+    class Renkaat
+    {
+        public string Merkki { get; set; }
+        public string Tyyppi { get; set; }
+        public string Koko { get; set; }
+        public Renkaat(string merkki, string tyyppi, string koko)
+        {
+            Merkki = merkki;
+            Tyyppi = tyyppi;
+            Koko = koko;
+        }
+        public override string ToString()
+        {
+            return "Merkki: " + Merkki + ", Tyyppi: " + Tyyppi + ", Koko: " + Koko;
+        }
+    }
+    class Kulkuneuvo
+    {
+        public string Nimi { get; set; }
+        public string Malli { get; set; }
+        public Kulkuneuvo(string nimi, string malli)
+        {
+            Nimi = nimi;
+            Malli = malli;
+        }
+        public override string ToString()
+        {
+            return "Nimi: " + Nimi + ", Malli: " + Malli;
+        }
+    }
+    class Tht01
+    {
+        static void Tehtava1()
+        {
+            Kulkuneuvo a = new Kulkuneuvo("Porsche", "911");
+            Renkaat a1 = new Renkaat("Nokia", "Hakkapeliitta", "205R16");
+            Console.WriteLine("Uusi auto luotu:\n{0} ja siihen uudet renkaat {1}", a, a1);
+            Kulkuneuvo b = new Kulkuneuvo("Ducati", "Diavel");
+            Renkaat b1 = new Renkaat("MIC", "Pilot", "160R17");
+            Console.WriteLine("Uusi moottoripyörä luotu\n{0} ja siihen uudet renkaat: {1}", b, b1);
+        }
+    }
+}
---
+++ Labra6/Tht02.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Labra6
+{
+    class Jaakaappi
+    {
+        public string Valmistaja { get; set; }
+        public string Malli { get; set; }
+        public string Elintarvikkeet { get; set; }
+        public Jaakaappi(string valmistaja, string malli, string elintarvikkeet)
+        {
+            Valmistaja = valmistaja;
+            Malli = malli;
+            Elintarvikkeet = elintarvikkeet;
+        }
+        public override string ToString()
+        {
+            return "\nValmistaja: " + Valmistaja + "\nMalli: " + Malli + "\nSisällä: " + Elintarvikkeet;
+        }
+    }
+    class Tht02
+    {
+        static void Main()
+        {
+            Jaakaappi jaakaappi = new Jaakaappi("Samsung", "BRB6000", "Juustoa, kinkkuleikettä, margariinia, ruisleipää, maitoa, tuoremehua");
+            Console.WriteLine("Jääkaappini {0}", jaakaappi);
+        }
+    }
+}
