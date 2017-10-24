630793a07389c156610c9f0df1f3eb76905819a6
K2177 <K2177@student.jamk.fi>
2017-10-25 02:27:15 +0300

    2+3

---
+++ Labra7/Tht02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labra7
+{
+    class CD
+    {
+        #region PROPERTIES
+        public string Nimi { get; set; }
+        public string Artist { get; set; }
+        public string Songs { get; set; }
+        #endregion
+        #region CONSTRUCTORS
+        public CD(string nimi, string artist, string songs)
+        {
+            Nimi = nimi;
+            Artist = artist;
+            Songs = songs;
+        }
+        #endregion
+        #region METHODS
+        public override string ToString()
+        {
+            return string.Format("Levy: {0}\nArtisti: {1}\nKappaleet: {2}\n", Nimi, Artist, Songs);
+        }
+        #endregion
+    }
+    class Tht02
+    {
+        static void Tehtava2()
+        {
+            List<CD> levy = new List<CD>();
+            levy.Add(new CD("Sounds of a Playground Fading", "In Flames", "Kappale1, 4:00 kappale2 4:00, kappale3 4:00"));
+            levy.Add(new CD("Indestructible", "Disturbed", "Kappale1, 2:00 kappale2 2:00, kappale3 2:00"));
+            levy.Add(new CD("Reload", "Metallica", "Kappale1, 3:00 kappale2 3:00, kappale3 3:00"));
+            Console.WriteLine("Jukeboksin levyt");
+            foreach (var o in levy)
+            {
+                Console.WriteLine(o.ToString());
+            }
+        }
+    }
+}
---
+++ Labra7/Tht03.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labra7
+{
+    class Deck
+    {
+        public string Kortti { get; set; }
+        public Deck(string kortti)
+        {
+            Kortti = kortti;
+        }
+        public override string ToString()
+        {
+            return string.Format(Kortti);
+        }
+    }
+    class Tht03
+    {
+        static void Main()
+        {
+            List<Deck> kortit = new List<Deck>();
+            kortit.Add(new Deck("Club#1"));
+            kortit.Add(new Deck("Club#2"));
+            kortit.Add(new Deck("Club#3"));
+            kortit.Add(new Deck("Club#4"));
+            kortit.Add(new Deck("Club#5"));
+            kortit.Add(new Deck("Club#6"));
+            kortit.Add(new Deck("Club#7"));
+            kortit.Add(new Deck("Club#8"));
+            kortit.Add(new Deck("Club#9"));
+            kortit.Add(new Deck("Club#10"));
+            kortit.Add(new Deck("Club#11"));
+            kortit.Add(new Deck("Club#12"));
+            kortit.Add(new Deck("Club#13"));
+            kortit.Add(new Deck("Heart#1"));
+            kortit.Add(new Deck("Heart#2"));
+            kortit.Add(new Deck("Heart#3"));
+            kortit.Add(new Deck("Heart#4"));
+            kortit.Add(new Deck("Heart#5"));
+            kortit.Add(new Deck("Heart#6"));
+            kortit.Add(new Deck("Heart#7"));
+            kortit.Add(new Deck("Heart#8"));
+            kortit.Add(new Deck("Heart#9"));
+            kortit.Add(new Deck("Heart#10"));
+            kortit.Add(new Deck("Heart#11"));
+            kortit.Add(new Deck("Heart#12"));
+            kortit.Add(new Deck("Heart#13"));
+            kortit.Add(new Deck("Diamond#1"));
+            kortit.Add(new Deck("Diamond#2"));
+            kortit.Add(new Deck("Diamond#3"));
+            kortit.Add(new Deck("Diamond#4"));
+            kortit.Add(new Deck("Diamond#5"));
+            kortit.Add(new Deck("Diamond#6"));
+            kortit.Add(new Deck("Diamond#7"));
+            kortit.Add(new Deck("Diamond#8"));
+            kortit.Add(new Deck("Diamond#9"));
+            kortit.Add(new Deck("Diamond#10"));
+            kortit.Add(new Deck("Diamond#11"));
+            kortit.Add(new Deck("Diamond#12"));
+            kortit.Add(new Deck("Diamond#13"));
+            kortit.Add(new Deck("Spade#1"));
+            kortit.Add(new Deck("Spade#2"));
+            kortit.Add(new Deck("Spade#3"));
+            kortit.Add(new Deck("Spade#4"));
+            kortit.Add(new Deck("Spade#5"));
+            kortit.Add(new Deck("Spade#6"));
+            kortit.Add(new Deck("Spade#7"));
+            kortit.Add(new Deck("Spade#8"));
+            kortit.Add(new Deck("Spade#9"));
+            kortit.Add(new Deck("Spade#10"));
+            kortit.Add(new Deck("Spade#11"));
+            kortit.Add(new Deck("Spade#12"));
+            kortit.Add(new Deck("Spade#13"));
+            foreach (var o in kortit)
+            {
+                Console.WriteLine(o.ToString());
+            }
+        }
+    }
+}
