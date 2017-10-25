d704f02029d64204b1a91bef4d3de7b01c31d46f
K2177 <K2177@student.jamk.fi>
2017-10-25 08:03:12 +0300

    .

--- Labra7/Tht02.cs
+++ Labra7/Tht02.cs
@@ -33,7 +33,7 @@ namespace Labra7
             levy.Add(new CD("Sounds of a Playground Fading", "In Flames", "Kappale1, 4:00 kappale2 4:00, kappale3 4:00"));
             levy.Add(new CD("Indestructible", "Disturbed", "Kappale1, 2:00 kappale2 2:00, kappale3 2:00"));
             levy.Add(new CD("Reload", "Metallica", "Kappale1, 3:00 kappale2 3:00, kappale3 3:00"));
-            Console.WriteLine("Jukeboksin levyt");
+            Console.WriteLine("Jukeboksi");
             foreach (var o in levy)
             {
                 Console.WriteLine(o.ToString());
