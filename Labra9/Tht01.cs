8a2a6a9cfcb8457c6ed3019c3fabbbd62a7ed840
K2177 <K2177@student.jamk.fi>
2017-11-08 03:00:53 +0200

    tht1+2

---
+++ Labra9/Tht01.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Labra9
+{
+    class Dice
+    {
+        public int Throws { get; set; }
+        public int Value { get; set; }
+        private float Average;
+        private int[] Count = new int[6];
+        Random rnd = new Random();
+        public void Throw()
+        {
+            Console.Write("Amount of throws: ");
+            Throws = int.Parse(Console.ReadLine());
+        }
+        public void Randomize()
+        {
+            for (int i = 0; i < Throws; i++)
+            {
+                Value = rnd.Next(1, 7);
+                Average += Value;
+                switch (Value)
+                {
+                    case 1: Count[0]++; break;
+                    case 2: Count[1]++; break;
+                    case 3: Count[2]++; break;
+                    case 4: Count[3]++; break;
+                    case 5: Count[4]++; break;
+                    case 6: Count[5]++; break;
+                    default:
+                        break;
+                }
+            }
+            Average = Average / Throws;
+        }
+        public override string ToString()
+        {
+            return "Average: " + Average + "\nThrows: " + Throws + "\n1 count: " + Count[0] + "\n2 count: " + Count[1] + "\n3 count: " + Count[2] + "\n4 count: " + Count[3] + "\n5 count: " + Count[4] + "\n6 count: " + Count[5];
+        }
+    }
+    class Tht01
+    {
+        static void Tehtava1()
+        {
+            Dice dice = new Dice();
+            dice.Throw();
+            dice.Randomize();
+            Console.WriteLine(dice.ToString());
+        }
+    }
+}
---
+++ Labra9/Tht02.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labra9
+{
+    class Product
+    {
+        public string Cart { get; set; }
+        public double Price { get; set; }
+        public Product(string cart, double price)
+        {
+            Cart = cart;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return "- " + Cart + " " + Price + "e";
+        }
+    }
+    class Tht02
+    {
+        static void Tehtava2()
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Milk", 1.4));
+            list.Add(new Product("Coca-Cola", 2.2));
+            list.Add(new Product("Butter", 3.2));
+            list.Add(new Product("Cheese", 4.2));
+            Console.WriteLine("All products:");
+            foreach (var l in list)
+            {
+                Console.WriteLine(l.ToString());
+            }
+        }
+    }
+}
