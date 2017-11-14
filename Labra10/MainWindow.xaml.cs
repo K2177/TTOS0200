0e57856d512425f5076982f348292be733d3f84e
K2177 <K2177@student.jamk.fi>
2017-11-15 01:15:31 +0200

    10tht1

---
+++ Labra10/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Labra10
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int cars;
+        int trucks;
+        private void Trucks_Click(object sender, RoutedEventArgs e)
+        {
+            trucks++;
+            TrucksNum.Text = trucks.ToString();
+        }
+        private void Cars_Click(object sender, RoutedEventArgs e)
+        {
+            cars++;
+            CarsNum.Text = cars.ToString();
+        }
+    }
+}
