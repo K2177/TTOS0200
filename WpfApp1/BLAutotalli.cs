0768293cf4db9a34f2b12d7650a09fa071ee8b67
K2177 <K2177@student.jamk.fi>
2017-11-15 11:03:49 +0200

    Demoo

---
+++ WpfApp1/BLAutotalli.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public static class BLAutotalli
+    {
+        public static List<Auto> HaeAutot()
+        {
+            List<Auto> autot = new List<Auto>();
+            //GUIn testaamista varten keksitään muutama auto
+            Auto a = new Auto();
+            a.Merkki = "Volvo";
+            a.Malli = "V70";
+            a.VM = 2007;
+            a.KM = 200000;
+            a.Hinta = 9000F;
+            a.URL = "VolvoV70.png";
+            autot.Add(a);
+            Auto b = new Auto() { Merkki = "Audi", Malli = "A4", VM = 2010, KM = 100000, Hinta = 19900, URL="AudiA4.png" };
+            autot.Add(b);
+            Auto c = new Auto() { Merkki = "Saab", Malli = "99", VM = 2000, KM = 1000000, Hinta = 900 };
+            autot.Add(c);
+            //Palautus
+            return autot;
+        }
+    }
+    public class Auto
+    {
+        public string Merkki { get; set; }
+        public string Malli { get; set; }
+        public int VM { get; set; }
+        public int KM { get; set; }
+        public float Hinta { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private List<Auto> autot;
+        private const string polku = @"D:\K2177\TTOS0200\\";
+        public MainWindow()
+        {
+            //Tänne koodi joka suoritetaan ikkunan avauksen luonnissa
+            InitializeComponent();
+            autot = BLAutotalli.HaeAutot();
+            NaytaKuva("autotalli.png");
+            //Asetetaan comboboxiin kaikki eri automerkit
+            //Tapa 1
+            List<string> merkit = new List<string>();
+            merkit.Add("Audi");
+            merkit.Add("Saab");
+            merkit.Add("Volvo");
+            cmbAutot.ItemsSource = merkit;
+            //Tapa 2
+            var result = autot.Select(n => n.Merkki.Distinct());
+            cmbAutot.ItemsSource = result;
+        }
+        private void btnHaeAutot_Click(object sender, RoutedEventArgs e)
+        {
+            dgAutot.ItemsSource = autot;
+        }
+        private void NaytaKuva(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                url = "puuttuu.png";
+            }
+            //Kuvatiedostojen vakiopolku
+            url = polku + url;
+            //Kuvan näyttö
+            BitmapImage pic = new BitmapImage();
+            pic.BeginInit();
+            pic.UriSource = new Uri(url);
+            pic.EndInit();
+            imgAuto.Stretch = Stretch.Fill;
+            imgAuto.Source = pic;
+        }
+        private void dgAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object obj = dgAutot.SelectedItem;
+            if (obj != null)
+            {
+                Auto valittu = (Auto)obj;
+                NaytaKuva(valittu.URL);
+            }
+        }
+        private void btnHaeAudit_Click(object sender, RoutedEventArgs e)
+        {
+            var result = autot.Where(n => n.Merkki.Contains("Audi"));
+            dgAutot.ItemsSource = result;
+        }
+        private void cmbAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Suodatetaan datagridiin näkyviin vain valittu automerkki
+            string merkki = cmbAutot.SelectedValue.ToString();
+            var result = autot.Where(n => n.Merkki.Contains(merkki));
+            dgAutot.ItemsSource = result;
+        }
+    }
+}
