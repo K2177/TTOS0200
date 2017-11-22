463cebb43a96f736c7e35a37cebe03ce05713445
K2177 <K2177@student.jamk.fi>
2017-11-22 03:27:16 +0200

    Labra11 Tht 1

---
+++ Labra11T1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labra11T1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Buy_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Milk.IsChecked)
+            {
+                List.Text += "Milk\n";
+            }
+            if ((bool)Butter.IsChecked)
+            {
+                List.Text += "Butter\n";
+            }
+            if ((bool)Chicken.IsChecked)
+            {
+                List.Text += "Chicken\n";
+            }
+            if ((bool)Beer.IsChecked)
+            {
+                List.Text += "Beer\n";
+            }
+            if ((bool)Lemonade.IsChecked)
+            {
+                List.Text += "Lemonade\n";
+            }
+        }
+    }
+}
