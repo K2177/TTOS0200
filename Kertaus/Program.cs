5e71b30b43044aa1d41499d6d006b921c8a4d7a4
K2177 <K2177@student.jamk.fi>
2017-11-22 11:27:28 +0200

    Kertaustht aloittelua

---
+++ Kertaus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kertaus
+{
+    class AMK
+    {
+        static void Main()
+        {
+
+        }
+    }
+    class Opiskelija
+    {
+
+    }
+    class Opettaja
+    {
+
+    }
+    class Tutkinto
+    {
+
+    }
+    class Opintojakso
+    {
+
+    }
+}
