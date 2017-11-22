8a1b560e77b4ff9937ec2593b88185cec7b4528d
K2177 <K2177@student.jamk.fi>
2017-11-22 03:42:09 +0200

    Puhdistelua

--- Labra11T1/MainWindow.xaml.cs
+++ Labra11T1/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Labra11T1
 {
@@ -23,6 +10,7 @@ namespace Labra11T1
         }
         private void Buy_Click(object sender, RoutedEventArgs e)
         {
+            List.Clear();
             if ((bool)Milk.IsChecked)
             {
                 List.Text += "Milk\n";
@@ -41,7 +29,7 @@ namespace Labra11T1
             }
             if ((bool)Lemonade.IsChecked)
             {
-                List.Text += "Lemonade\n";
+                List.Text += "Lemonade";
             }
         }
     }
